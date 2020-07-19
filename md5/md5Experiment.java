a6400d7bae794b02e5c3558635e0ecb1e7b5fe46
Reza <rs@mage.me.uk>
2020-07-19 21:32:43 +0100

    Java version of md5

---
+++ md5/md5Experiment.java
@@ -0,0 +1,31 @@
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
+
+import java.math.BigInteger;
+ 
+
+class md5Experiment {
+    public static int getDatabaseID(String domainName, int totalShards) {
+        byte[] bytesOfDomainName = domainName.getBytes(StandardCharsets.UTF_8);
+
+        try {
+          MessageDigest md = MessageDigest.getInstance("MD5");
+          byte[] md5Digest = md.digest(bytesOfDomainName);
+          BigInteger parameterInt = new BigInteger(1, md5Digest);
+          BigInteger modulus = new BigInteger(Integer.toString(totalShards));
+          BigInteger shard_id = parameterInt.mod(modulus);
+          return shard_id.intValue();
+        }
+        catch (NoSuchAlgorithmException e) {
+          System.err.println("I'm sorry, but MD5 is not a valid message digest algorithm. Exiting..");
+          System.exit(1);
+        }
+
+        return 9999;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("database id: " + getDatabaseID("example.com", 3));
+    }
+}
